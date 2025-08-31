c0a9ec36cab5ea3760dcc6be00ba9ac60716637a
Axel-SOE <axelsoebi@gmail.com>
2025-08-31 15:21:26 +0200

    Add files via upload

---
+++ javascript/script_jaar.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function () {
+  // Leaflet map code here...
+
+  // Footer year
+  document.getElementById('year').textContent = new Date().getFullYear();
+});
