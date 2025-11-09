416b6ab58cfa3a94ef6b98f02764149bbf20ff0c
Axel-SOE <axelsoebi@gmail.com>
2025-11-09 13:51:35 +0100

    afgewerkt

--- javascript/script_jaar.js
+++ javascript/script_jaar.js
@@ -1,5 +1,4 @@
 document.addEventListener('DOMContentLoaded', function () {
-  // Leaflet map code here...
 
   // Footer year
   document.getElementById('year').textContent = new Date().getFullYear();
