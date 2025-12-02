da663d480f1973d0faa3f3bef4018d74946d91cb
Axel-SOE <axelsoebi@gmail.com>
2025-12-02 23:21:20 +0100

    Add files via upload

--- javascript/map.js
+++ javascript/map.js
@@ -7,16 +7,8 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-L.polygon([
-    [51.29774349503758, 4.440256466514076],
-    [51.306973401874664, 4.4678939490561245],
-    [51.31995652344387, 4.464460721411149],
-    [51.351165353312034, 4.518877379584001],
-    [51.307617279600436, 4.558359497501213],
-    [51.26896862417096, 4.460512509619428]
-],{
-    color: '#49C3EB',
-    fillOpacity: 0.3
-}).addTo(map);
+var brasschaatCoords = [51.29014396111914, 4.49155569097763];
+var marker = L.marker(brasschaatCoords).addTo(map);
+marker.bindPopup('<b>Brasschaat</b><br>2930');
 });
 
